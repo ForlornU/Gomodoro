ab7a4cc15f06f7b498f35baa4f06975abd179543
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-08-23 18:43:01 +0200

    wip logic

--- ButtonLogic.gd
+++ ButtonLogic.gd
@@ -1,17 +1,24 @@
 extends Control
 
 @onready var color_rect: ColorRect = $ColorRect
+@onready var timer_label: Label = $ColorRect/TimerLabel
+var timer : float = 0.0
+var timer_status = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	timer_label.text = "00:00"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	timer += delta
+	timer_label.text = str(timer)
 
 func _button_pressed() -> void:
-	color_rect.color = Color.RED
-	pass # Replace with function body.
+
+	timer_status = !timer_status
+	
+	if(timer_status == true):
+		color_rect.color = Color.RED
+	else:
+		color_rect.color = Color.FOREST_GREEN
