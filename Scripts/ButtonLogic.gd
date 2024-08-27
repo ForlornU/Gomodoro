666430c99a2ce2a2e0b2f6a1c8d57be09f1e2958
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-08-27 18:08:42 +0200

    new global changes

--- Scripts/ButtonLogic.gd
+++ Scripts/ButtonLogic.gd
@@ -1,14 +1,8 @@
 extends Control
 
 #State
-enum State {WORK, BREAK, PAUSED}
-var current_state : State
-var previous_state : State
-
-#Time
-var work_time = 1800 #Default 30min
-var short_pause_time = 300 # 5min
-var cycles = 4 #How many work/break cycles before complete
+var current_state : Global.State
+var previous_state : Global.State
 
 #Preloaded Audio
 const BACK_TO_WORK = preload("res://Assets/Audio/BackToWork.wav")
@@ -25,7 +19,7 @@ const TAKE_A_BREAK = preload("res://Assets/Audio/TakeABreak.wav")
 #endregion
 
 func _ready() -> void:
-	current_state = State.PAUSED
+	current_state = Global.State.WORK
 	get_window().always_on_top = true
 	settings_panel.visible = false
 	timer_label.text = "30:00"
@@ -41,54 +35,48 @@ func _process(_delta: float) -> void:
 			timer_label.text = str(seconds)
 		else:
 			timer_label.text = str(minutes) + ":" + str(seconds)
-	texture_progress_bar.value = work_time - timer.time_left
+	texture_progress_bar.value = Global.work_time - timer.time_left
 
-func switch_state(state : State):
-	if(current_state == state):
-		print("ignoring same-state transition")
-		return
-	
+func switch_state(new_state : Global.State, from_pause : bool):
 	previous_state = current_state
-	match state:
-		State.WORK:
+	match new_state:
+		Global.State.WORK:
+			if(!from_pause):
+				timer.start(Global.work_time)
+				audio_stream_player.stream = BACK_TO_WORK
+				audio_stream_player.play()
+			texture_progress_bar.max_value = Global.work_time
 			circle.self_modulate = Color.DARK_RED
 			texture_progress_bar.tint_progress = Color.RED
 			status_label.text = "Work!"
-			audio_stream_player.stream = BACK_TO_WORK
-			audio_stream_player.play()
-		State.BREAK:
-			reset_timer()
+		Global.State.BREAK:
+			if(!from_pause):
+				timer.start(Global.short_pause_time)
+				audio_stream_player.stream = TAKE_A_BREAK
+				audio_stream_player.play()
+			texture_progress_bar.max_value = Global.short_pause_time
 			circle.self_modulate = Color.SEA_GREEN
 			texture_progress_bar.tint_progress = Color.SEA_GREEN
 			status_label.text = "Relax"
-			audio_stream_player.stream = TAKE_A_BREAK
-			audio_stream_player.play()
-		State.PAUSED:
+		Global.State.PAUSED:
 			circle.self_modulate = Color.YELLOW
 			texture_progress_bar.tint_progress = Color.YELLOW
 			status_label.text = "Paused"
-	current_state = state
+			
+	current_state = new_state
 
 #Switching state should always start timer, if not pause
 func resume():
-	if(timer.is_stopped()): #This only happens at start, otherwise its just paused
-		timer.start()
 	if(timer.paused == true):
 		timer.paused = false
-
-#Start timer with new time
-func reset_timer():
-	timer.start(work_time)
-	texture_progress_bar.max_value = work_time
 
 #region Buttons
 #After the timer runs out, switch state
 func _on_timer_timeout() -> void:
-	reset_timer()
-	if(current_state == State.WORK):
-		switch_state(State.BREAK)
-	elif(current_state == State.BREAK):
-		switch_state(State.WORK)
+	if(current_state == Global.State.WORK):
+		switch_state(Global.State.BREAK, false)
+	elif(current_state == Global.State.BREAK):
+		switch_state(Global.State.WORK, false)
 
 func _on_settings_button_pressed() -> void:
 	settings_panel.visible = !settings_panel.visible
@@ -100,32 +88,28 @@ func _on_timevalue_changed(value: int) -> void:
 	print("New timer set to: " + str(value))
 	var new_time : int = 60*value
 	timer.wait_time = new_time
-	work_time = new_time
+	Global.work_time = new_time
 	timer_label.text = str(timer.wait_time)
 
 func _on_pause() -> void:
 	timer.paused = !timer.paused
 	if(timer.paused == false):
-		switch_state(previous_state)
+		switch_state(previous_state, true)
 	else:
-		switch_state(State.PAUSED)
-
-#Start break
-func _take_break_button() -> void:
-	if(current_state == State.BREAK):
-		return
-	reset_timer()
-	resume()
-	switch_state(State.BREAK)
+		switch_state(Global.State.PAUSED, false)
 
 func _on_sound_check_toggled(toggled_on: bool) -> void:
 	audio_stream_player.volume_db = 0 if toggled_on else -80
 	
 func _next_state_pressed() -> void:
-	if(current_state == State.WORK):
-		return
-	reset_timer()
 	resume()
-	switch_state(State.WORK)
+	if(timer.is_stopped()): #First time running
+		switch_state(Global.State.WORK, false)
+		return
+		
+	if(current_state == Global.State.WORK):
+		switch_state(Global.State.BREAK, false)
+	elif(current_state == Global.State.BREAK):
+		switch_state(Global.State.WORK, false)
 
 #endregion
