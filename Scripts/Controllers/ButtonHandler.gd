42ad178b3725f4ca1855fe4c329be25e216b79c3
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-09-02 20:39:29 +0200

    Volume Slider

--- Scripts/Controllers/ButtonHandler.gd
+++ Scripts/Controllers/ButtonHandler.gd
@@ -5,9 +5,6 @@ extends Node
 func _on_always_ontop(toggled_on: bool) -> void:
 	get_window().always_on_top = toggled_on
 
-func _on_sound_check_toggled(toggled_on: bool) -> void:
-	audio_stream_player.volume_db = 0 if toggled_on else -80
-
 #Changing Durations
 func _on_worktime_changed(value: float) -> void:
 	var new_time : int = 60*value
@@ -16,3 +13,6 @@ func _on_worktime_changed(value: float) -> void:
 func _on_pausetime_changed(value: float) -> void:
 	var new_time : int = 60*value
 	Global.short_pause_duration = new_time
+
+func _on_volume_changed(value: float) -> void:
+	audio_stream_player.volume_db = value
